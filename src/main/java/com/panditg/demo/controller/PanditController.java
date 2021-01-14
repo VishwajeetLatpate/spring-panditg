7aebb062ce3288161ced9f3a38b2b23e9275e816
VishwajeetLatpate <vlatpate@gmail.com>
2021-01-14 21:32:12 +0530

    getPandit

--- src/main/java/com/panditg/demo/controller/PanditController.java
+++ src/main/java/com/panditg/demo/controller/PanditController.java
@@ -3,6 +3,7 @@ package com.panditg.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.panditg.demo.entities.Pandit;
 import com.panditg.demo.model.PanditModel;
 import com.panditg.demo.service.PanditService;
-
+@CrossOrigin
 @RestController
 public class PanditController {
 
